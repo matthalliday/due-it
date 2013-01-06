0cee0aff3980c77bc0d17bef2ca54fce879e003c
Matt Halliday <matthalliday@gmail.com>
2013-01-06 15:28:46 -0500

    Create seed data and silence debug output

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+project = Project.new
+project.assign_attributes({name: 'Sample Project',
+                           description: 'A sample project for demo purposes'},
+                           without_protection: true)
+project.save
+
+task1 = Task.new
+task1.assign_attributes({project_id: 1,
+                         name: 'Your first task',
+                         description: 'A sample task for demo purposes',
+                         priority: 'normal',
+                         estimate: '5 minutes',
+                         due_date: 30.days.from_now},
+                         without_protection: :true)
+task1.save
+
+task2 = Task.new
+task2.assign_attributes({project_id: 1,
+                         name: 'Your second task',
+                         description: 'Another sample task for demo purposes',
+                         priority: 'high',
+                         estimate: '2 days',
+                         due_date: 4.days.from_now},
+                         without_protection: :true)
+task2.save
