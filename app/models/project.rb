379c2db93c174a64000a02863fd21770f2b0acaa
Matt Halliday <matthalliday@gmail.com>
2013-01-06 16:35:24 -0500

    Define more validations for projects and tasks

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,13 @@ class Project < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
 
   attr_accessible :description, :name
-  validates_presence_of :name
+
+  validates :description, length: { maximum: 500 }
+  validates :name, presence: :true,
+                   uniqueness: :true,
+                   length: { in: 2..64 }
+
+  def to_param
+    [id, name.parameterize].join('-')
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,11 @@ class Task < ActiveRecord::Base
   belongs_to :project
 
   attr_accessible :description, :due_date, :estimate, :name, :priority
-  validates_presence_of :due_date, :priority, :name
+
+  validates :description, length: { maximum: 500 }
+  validates :due_date, presence: :true
+  validates :priority, presence: :true,
+                       inclusion: { in: %w(low normal high) }
+  validates :name, presence: :true,
+                   length: { maximum: 256 }
 end
