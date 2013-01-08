95daa0cccbfb515c4f243726ac26f7597295e243
Matt Halliday <matthalliday@gmail.com>
2013-01-07 21:38:53 -0500

    Add status column to attr_accessible and add named scopes based on status

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,16 +1,16 @@
 class Task < ActiveRecord::Base
   belongs_to :project
 
-  attr_accessible :description, :due_date, :estimate, :name, :priority
+  attr_accessible :description, :due_date, :estimate, :name, :priority, :status
 
   validates :description, length: { maximum: 500 }
   validates :due_date, presence: :true
-  validates :priority, presence: :true,
-                       inclusion: { in: %w(low normal high) }
-  validates :name, presence: :true,
-                   length: { maximum: 256 }
+  validates :name, presence: :true, length: { maximum: 256 }
+  validates :priority, presence: :true, inclusion: { in: %w(low normal high) }
 
-  scope :due_this_week, -> { where(due_date: (Time.now)..(1.week.from_now)) }
-  scope :due_next_week, -> { where(due_date: (1.week.from_now)..(2.weeks.from_now)) }
-  scope :due_this_month, -> { where(due_date: (Time.now)..(1.month.from_now)) }
+  scope :complete, -> { where(status: 'complete') }
+  scope :incomplete, -> { where(status: 'incomplete') }
+  scope :this_week, -> { where(due_date: (Time.now)..(1.week.from_now)) }
+  scope :next_week, -> { where(due_date: (1.week.from_now)..(2.weeks.from_now)) }
+  scope :this_month, -> { where(due_date: (Time.now)..(1.month.from_now)) }
 end
