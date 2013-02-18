b10f3fb9969aca909cad19cb54faacf40ca9ed3a
Matt Halliday <matthalliday@gmail.com>
2013-02-18 15:11:18 -0500

    Add remember me functionality to user log in and sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      login(params[:user][:username], params[:user][:password])
+      login(params[:user][:username], params[:user][:password], 1)
       redirect_to root_url
       flash[:success] = "Sign up successful!"
     else
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130218003811) do
+ActiveRecord::Schema.define(:version => 20130218190647) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -38,8 +38,12 @@ ActiveRecord::Schema.define(:version => 20130218003811) do
     t.string   "email"
     t.string   "crypted_password"
     t.string   "salt"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at",                   :null => false
+    t.datetime "updated_at",                   :null => false
+    t.string   "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
   end
+
+  add_index "users", ["remember_me_token"], :name => "index_users_on_remember_me_token"
 
 end
