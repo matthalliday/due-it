d42408a26e6511cbc2f8961c4b9a12e5b068cac8
Matt Halliday <matthalliday@gmail.com>
2013-05-03 20:28:54 -0400

    Convert indentation from tabs to spaces for user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,13 +16,13 @@ describe User do
 
   it { should validate_confirmation_of(:password) }
 
-	it { should ensure_length_of(:username).is_at_most(16) }
+  it { should ensure_length_of(:username).is_at_most(16) }
   it { should ensure_length_of(:password).is_at_least(4) }
 
   it { should_not allow_mass_assignment_of(:salt) }
-	it { should_not allow_mass_assignment_of(:crypted_password) }
-	it { should_not allow_mass_assignment_of(:remember_me_token) }
-	it { should_not allow_mass_assignment_of(:reset_password_token) }
-	it { should_not allow_mass_assignment_of(:activation_state) }
-	it { should_not allow_mass_assignment_of(:activation_code) }
+  it { should_not allow_mass_assignment_of(:crypted_password) }
+  it { should_not allow_mass_assignment_of(:remember_me_token) }
+  it { should_not allow_mass_assignment_of(:reset_password_token) }
+  it { should_not allow_mass_assignment_of(:activation_state) }
+  it { should_not allow_mass_assignment_of(:activation_code) }
 end
