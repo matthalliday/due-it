49296c9d2e3e97bd33e7ed733a1393a67e793fe9
Matt Halliday <matthalliday@gmail.com>
2013-05-05 16:48:01 -0400

    Add more test coverage to Project model
    Specs now ensure that URL param contains project name and default
    ordering is honored

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Project do
   it { should belong_to(:user) }
-  it { should have_many(:tasks) }
+  it { should have_many(:tasks).dependent(:destroy) }
 
   it { should ensure_length_of(:description).is_at_most(500) }
   it { should ensure_length_of(:name).is_at_least(2).is_at_most(64) }
@@ -13,4 +13,16 @@ describe Project do
   it { should_not allow_mass_assignment_of(:complete_tasks) }
   it { should_not allow_mass_assignment_of(:incomplete_tasks) }
   it { should_not allow_mass_assignment_of(:user_id) }
+
+  it "should include id and project name in URL param" do
+    project = Project.create!(id: 1, name: 'Test Project')
+    project.to_param.should == '1-test-project'
+  end
+
+  it "should be ordered alphabetically by default" do
+    project1 = Project.create!(name: 'Interesting Project')
+    project2 = Project.create!(name: '1st Rails Project')
+    project3 = Project.create!(name: 'Awesome Project')
+    Project.all.should == [project2, project3, project1]
+  end
 end
