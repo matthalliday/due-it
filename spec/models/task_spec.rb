04d97634e5345d1531fa4ad9a7f411272cea6d55
Matt Halliday <matthalliday@gmail.com>
2013-05-05 20:19:41 -0400

    Add default order spec for task model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -11,4 +11,11 @@ describe Task do
 
   it { should_not allow_mass_assignment_of(:project_id) }
   it { should_not allow_mass_assignment_of(:status) }
+
+  it "should be ordered by due date by default" do
+    task1 = Task.create!(name: 'Task 1', due_date: Time.now)
+    task2 = Task.create!(name: 'Task 2', due_date: Time.now + 2.weeks)
+    task3 = Task.create!(name: 'Task 3', due_date: Time.now.yesterday)
+    Task.all.should == [task3, task1, task2]
+  end
 end
