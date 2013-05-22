d9a7fdd20c72b4c062086a643c9b839eb95caa6c
Matt Halliday <matthalliday@gmail.com>
2013-05-22 00:14:06 -0400

    Create demo user when seed data is loaded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,31 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-project = Project.new
-project.assign_attributes({name: 'Sample Project',
-                           description: 'A sample project for demo purposes',
-                           incomplete_tasks: 2},
-                           without_protection: true)
-project.save
+# Don't send activation email
+User.after_create.clear
 
-task1 = Task.new
-task1.assign_attributes({project_id: 1,
-                         name: 'Your first task',
-                         description: 'A sample task for demo purposes',
-                         estimate: '5 minutes',
-                         due_date: 30.days.from_now},
-                         without_protection: :true)
-task1.save
+user = User.create({username: 'admin',
+                    email: 'you@example.com',
+                    password: 'secret'},
+                    without_protection: true)
+user.activate!
 
-task2 = Task.new
-task2.assign_attributes({project_id: 1,
-                         name: 'Your second task',
-                         description: 'Another sample task for demo purposes',
-                         estimate: '2 days',
-                         due_date: 4.days.from_now},
-                         without_protection: :true)
-task2.save
+Project.create({user_id: 1,
+                name: 'Sample Project',
+                description: 'A sample project for demo purposes',
+                incomplete_tasks: 2},
+                without_protection: true)
+
+Task.create({project_id: 1,
+             name: 'Your first task',
+             description: 'A sample task for demo purposes',
+             estimate: '5 minutes',
+             due_date: 30.days.from_now},
+             without_protection: :true)
+
+Task.create({project_id: 1,
+             name: 'Your second task',
+             description: 'Another sample task for demo purposes',
+             estimate: '2 days',
+             due_date: 4.days.from_now},
+             without_protection: :true)
