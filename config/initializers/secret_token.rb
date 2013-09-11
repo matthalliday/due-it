94469cb896666eb875eeedd33ecc9dfbe0c6f985
Matt Halliday <matthalliday@gmail.com>
2013-09-10 23:54:08 -0400

    Move secret token into environment variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DueIt::Application.config.secret_token = '3ac872d8e4a3d7f276374c4b3cb2ea96421857cd2ede5d1aeda5fdd9ea04b64fcdf501c127bdd8c3a1e5676923b3e7f145796bcf4ce06786ce15cb8099f8b44a'
+DueIt::Application.config.secret_token = ENV['SECRET_TOKEN']
