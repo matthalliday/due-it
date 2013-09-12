43a4cd3eb5e40875a139f1b32d613fea11c17010
Matt Halliday <matthalliday@gmail.com>
2013-09-11 22:47:42 -0400

    Upgrade Gemfile & app configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,12 @@ DueIt::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
+  # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both thread web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
+  config.eager_load = true
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -16,6 +22,9 @@ DueIt::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+
+  # Compress JavaScripts and CSS
+  config.assets.js_compressor = :uglifier
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -63,8 +72,4 @@ DueIt::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 DueIt::Application.config.secret_token = ENV['SECRET_TOKEN']
+DueIt::Application.config.secret_key_base = ENV['SECRET_TOKEN_BASE']
