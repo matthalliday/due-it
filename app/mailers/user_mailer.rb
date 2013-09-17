4ce919d477a91981f8e38d9f8210a61652f0494f
Matt Halliday <matthalliday@gmail.com>
2013-09-16 22:07:16 -0400

    Update user_mailer content

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,21 +3,21 @@ class UserMailer < ActionMailer::Base
 
   def activation_needed_email(user)
     @user = user
-    @subject = "Welcome to Due It"
+    @subject = "Due It: Please activate your account"
     @url = activate_user_url(user.activation_code)
     mail(to: user.email, subject: @subject)
   end
 
   def activation_success_email(user)
     @user = user
-    @subject = "Due It &ndash; Your account is now activated"
+    @subject = "Due It: Your account has been activated"
     @url = log_in_url
     mail(to: user.email, subject: @subject)
   end
 
   def reset_password_email(user)
     @user = user
-    @subject = "Due It &ndash; Your password reset request"
+    @subject = "Due It: Your password reset request"
     @url = edit_password_reset_url(user.reset_password_token)
     mail(to: user.email, subject: @subject)
   end
