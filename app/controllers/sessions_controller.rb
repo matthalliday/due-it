76ab70fcc9c49763c9adc3b7f80d070978d01fee
Matt Halliday <matthalliday@gmail.com>
2016-06-16 23:12:15 -0400

    Always set remember_me flag for user sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = login(params[:email], params[:password], params[:remember_me])
+    user = login(params[:email], params[:password], true)
     if user
       redirect_back_or_to root_url
       flash[:success] = "Logged in successfully"
