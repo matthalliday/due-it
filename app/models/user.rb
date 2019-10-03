764c539bb16588a1ae7a8cc797872a3fd2b23f02
Matt Halliday <matt.halliday@shopify.com>
2019-10-02 22:28:58 -0400

    Re-install Clearance gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
 
   has_many :projects, -> { order(name: :asc) }, dependent: :destroy
   has_many :tasks, through: :projects
+
+  # TODO: is this required by Clearance gem?
+  attr_accessor :username
 end
